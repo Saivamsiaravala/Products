ca8277adf7bb14e33ebaed97dece49a82e6e8567
Vamsi Aravala <saivamsiaravala9333@gmail.com>
2025-02-21 22:06:09 +0530

    second commit

--- src/Pages/Cart/CartSlice.tsx
+++ src/Pages/Cart/CartSlice.tsx
@@ -1,27 +1,26 @@
-import {  createSlice } from "@reduxjs/toolkit";
-interface StateType{
-    cartItems:number
-
+import { createSlice } from "@reduxjs/toolkit";
+interface StateType {
+  cartItems: number;
+  isEmpty: boolean;
 }
-const initialState:StateType={cartItems:0}
+const initialState: StateType = { cartItems: 0, isEmpty: true };
 
 const cartSlice = createSlice({
-      name:'cart',
-    initialState,
-    reducers:{
-        addItem:(state)=>{
-            return state.cartItems=state.cartItems+1
-        }
-        removeItem:(state)=>{
-            return state.cartItems=state.cartItems-1
-        }
-        clearCart:(state)=>{
-            return state.cartItems=0;
-        }
-    }   
-}
-  )
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state) => {
+      state.cartItems += 1;
+    },
+    removeItem: (state) => {
+      state.cartItems -= 1;
+    },
+    clearCart: (state) => {
+      (state.cartItems = 0), (state.isEmpty = true);
+    },
+  },
+});
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
 
-export const {addItem,returnItem,clearCart}= cartSlice.actions;
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
